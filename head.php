d355a277017139c9f1ae45f83c451f054d47ff6c
hanyong5 <hanyong5@naver.com>
2022-08-23 15:02:21 +0900

    theme v2

--- head.php
+++ head.php
@@ -36,40 +36,41 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
         <div id="logo">
             <a href="<?php echo G5_URL ?>"><img src="<?php echo G5_THEME_IMG_URL ?>/logo.svg" alt="<?php echo $config['cf_title']; ?>"></a>
         </div>
-    
+	
+		<nav id="gnb">
+			<h2>메인메뉴</h2>
+			<div class="gnb_wrap">
+				<ul id="gnb_1dul">
+					
+					<?php
+					$menu_datas = get_menu_db(0, true);
+					$gnb_zindex = 999; // gnb_1dli z-index 값 설정용
+					$i = 0;
+					foreach( $menu_datas as $row ){
+						if( empty($row) ) continue;
+						$add_class = (isset($row['sub']) && $row['sub']) ? 'gnb_al_li_plus' : '';
+					?>
+					<li class="gnb_1dli <?php echo $add_class; ?>" style="z-index:<?php echo $gnb_zindex--; ?>">
+						<a href="<?php echo $row['me_link']; ?>" target="_<?php echo $row['me_target']; ?>" class="gnb_1da"><?php echo $row['me_name'] ?></a>
+						
+					</li>
+					<?php
+					$i++;
+					}   //end foreach $row
+
+					if ($i == 0) {  ?>
+						<li class="gnb_empty">메뉴 준비 중입니다.<?php if ($is_admin) { ?> <a href="<?php echo G5_ADMIN_URL; ?>/menu_list.php">관리자모드 &gt; 환경설정 &gt; 메뉴설정</a>에서 설정하실 수 있습니다.<?php } ?></li>
+					<?php } ?>
+				</ul>
+				
+				
+			</div>
+    	</nav>
         
         
     </div>
     
-    <nav id="gnb">
-        <h2>메인메뉴</h2>
-        <div class="gnb_wrap">
-            <ul id="gnb_1dul">
-                
-                <?php
-				$menu_datas = get_menu_db(0, true);
-				$gnb_zindex = 999; // gnb_1dli z-index 값 설정용
-                $i = 0;
-                foreach( $menu_datas as $row ){
-                    if( empty($row) ) continue;
-                    $add_class = (isset($row['sub']) && $row['sub']) ? 'gnb_al_li_plus' : '';
-                ?>
-                <li class="gnb_1dli <?php echo $add_class; ?>" style="z-index:<?php echo $gnb_zindex--; ?>">
-                    <a href="<?php echo $row['me_link']; ?>" target="_<?php echo $row['me_target']; ?>" class="gnb_1da"><?php echo $row['me_name'] ?></a>
-                    
-                </li>
-                <?php
-                $i++;
-                }   //end foreach $row
-
-                if ($i == 0) {  ?>
-                    <li class="gnb_empty">메뉴 준비 중입니다.<?php if ($is_admin) { ?> <a href="<?php echo G5_ADMIN_URL; ?>/menu_list.php">관리자모드 &gt; 환경설정 &gt; 메뉴설정</a>에서 설정하실 수 있습니다.<?php } ?></li>
-                <?php } ?>
-            </ul>
-            
-            
-        </div>
-    </nav>
+    
 
 </div>
 <!-- } 상단 끝 -->
